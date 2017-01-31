e9ec2dba1ac26755fa7377e6d244eeec57d378d6
RusArtm <rusartm@gmail.com>
2017-01-31 22:01:58 +0700

    changed to Socket

--- src/ru/atomar/java/RequestUtils.java
+++ src/ru/atomar/java/RequestUtils.java
@@ -1,7 +1,56 @@
 package ru.atomar.java;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URL;
+
 /**
  * Created by RusAr on 31.01.2017.
  */
 public class RequestUtils {
+
+    public static String sendHttp1Request(String host, String file, String params) {
+        Socket socket;
+        PrintWriter out;
+        BufferedReader in;
+
+        StringBuffer result = new StringBuffer();
+        String line;
+
+        try {
+            socket = new Socket(host, 80);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String request = "POST " + file + " HTTP/1.0 \n" +
+                    "Host: " + host + "\n" +
+                    "Content-Type: application/x-www-form-urlencoded\n" +
+                    "Content-Length: " + String.valueOf(params.length()) + "\n" +
+                    "\n";
+
+            result.append("======================= REQUEST =======================\n");
+            result.append(request);
+
+            out.write(request);
+            out.flush();
+
+            result.append("======================= RESPONSE =======================\n");
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+                result.append('\n');
+            }
+
+            out.close();
+            in.close();
+            socket.close();
+
+            return result.toString();
+
+        } catch (IOException e) {
+            return e.toString();
+        }
+
+    }
+
 }
--- src/ru/atomar/java/RequestWindow.java
+++ src/ru/atomar/java/RequestWindow.java
@@ -1,7 +1,79 @@
 package ru.atomar.java;
 
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  * Created by RusAr on 31.01.2017.
  */
 public class RequestWindow {
+
+    Stage mStage;
+    TextArea mOutput;
+    TextField mUrl;
+    TextField mFile;
+    TextArea mContent;
+
+    public RequestWindow() {
+        BorderPane mainPane = new BorderPane();
+        Scene scene = new Scene(mainPane);
+
+        VBox vBox = new VBox();
+        HBox hBox;
+        Label label;
+
+        hBox = new HBox();
+        label = new Label();
+        label.setText("Host:");
+        mUrl = new TextField();
+        mUrl.setText("localhost");
+        mUrl.setPrefWidth(150);
+        hBox.getChildren().addAll(label, mUrl);
+        label = new Label();
+        label.setText("Res:");
+        mFile = new TextField();
+        mFile.setText("/");
+        mFile.setPrefWidth(150);
+        hBox.getChildren().addAll(label, mFile);
+        vBox.getChildren().add(hBox);
+
+        label = new Label();
+        label.setText("Content:");
+        mContent = new TextArea();
+        mContent.setPrefSize(800, 60);
+        vBox.getChildren().addAll(label, mContent);
+
+        Button sendButton = new Button("Send");
+        sendButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                mOutput.clear();
+                mOutput.setText(RequestUtils.sendHttp1Request(mUrl.getText(), mFile.getText(), mContent.getText()));
+            }
+        });
+        vBox.getChildren().add(sendButton);
+
+        label = new Label();
+        label.setText("Response:");
+        mOutput = new TextArea();
+        mOutput.setPrefSize(800, 600);
+        vBox.getChildren().addAll(label, mOutput);
+
+        mainPane.setCenter(vBox);
+
+        mStage = new Stage();
+        mStage.setScene(scene);
+
+        mStage.show();
+    }
+
 }
